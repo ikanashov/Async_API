4db4b262ab0285e20822f75dbbfba7fc44bba9d3
Ivan Kanashov <ikanashov@gmail.com>
2021-04-20 22:42:47 +0500

    dokerize functional test

---
+++ start_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd tests
+
+#sudo docker-compose --env-file ../.env up --build -d
+sudo docker-compose --env-file ../.env up --build
---
+++ stop_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd tests
+
+sudo docker-compose --env-file ../.env down
