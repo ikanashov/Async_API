7a25660aaa6f1e288b5e8379fd3d3402e58fa9f7
Ivan Kanashov <ikanashov@gmail.com>
2021-04-24 23:38:45 +0500

    add tests for get person by id

--- tests/functional/src/test_person_api.py
+++ tests/functional/src/test_person_api.py
@@ -15,20 +15,20 @@ async def test_person(make_get_request, read_json_data):
             assert response.body == await read_json_data('person/' + test['body']), test
         logger.info(f"{test['name']} passed")
 
-"""
+
 @pytest.mark.asyncio
-async def test_get_film_by_id(make_get_request, read_json_data):
-    testsconfig = await read_json_data('film/config_get_by_id.json')
+async def test_get_person_by_id(make_get_request, read_json_data):
+    testsconfig = await read_json_data('person/config_get_by_id.json')
     for test in testsconfig:
         logger.info(f"start test : {test['name']} ")
-        response = await make_get_request('film/' + test['film_id'])
+        response = await make_get_request('person/' + test['person_id'])
         assert response.status == int(test['status']), test
         assert len(response.body) == int(test['lenght']), test
         if test['body'] != '':
-            assert response.body == await read_json_data('film/' + test['body']), test
+            assert response.body == await read_json_data('person/' + test['body']), test
         logger.info(f"{test['name']} passed")
 
-
+"""
 @pytest.mark.asyncio
 async def test_film_search(make_get_request, read_json_data):
     testsconfig = await read_json_data('film/config_search.json')
