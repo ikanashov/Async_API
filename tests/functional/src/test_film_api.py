8ba39081de68c9b3bdc5a10f7b77b89d353e6b9e
Ivan Kanashov <ikanashov@gmail.com>
2021-04-24 18:07:32 +0500

    remove unused comments

--- tests/functional/src/test_film_api.py
+++ tests/functional/src/test_film_api.py
@@ -3,43 +3,23 @@ from loguru import logger
 import pytest
 
 
-# test get_all_film
-#   elastic pagination bigger than 10000 is not allowed
+#   For future development
+#   It's necessary to consider that
+#   elastic pagination bigger than 10000 is not allowed by default
 #   https://www.elastic.co/guide/en/elasticsearch/guide/current/pagination.html
 #   https://stackoverflow.com/questions/35206409/elasticsearch-2-1-result-window-is-too-large-index-max-result-window/35221900#35221900
 #   https://stackoverflow.com/questions/41677198/result-window-is-too-large-from-size-must-be-less-than-or-equal-to-10000-b
 #   curl -XPUT "http://localhost:9200/my_index/_settings" -d
 #   '{ "index" : { "max_result_window" : 500000 } }' -H "Content-Type: application/json"
-#   + test filter[genre]
-#   + test sort by imdb_rating
-#   test page[size]
-#      + size = default
-#       size = -1
-#       size = 0
-#       size = 1
-#       + size = 50
-#       size = 1000
-#   test page[number]
-#       + number = default
-#       + number = -1
-#       number = 0
-#       + number = 1
-#       number = 1000
-# + test search film
-# test get film by id
 
 @pytest.mark.asyncio
 async def test_film(make_get_request, read_json_data):
-    # Имя файла надо убрать в конфиг
     testsconfig = await read_json_data('film/config.json')
     for test in testsconfig:
         logger.info(f"start test : {test['name']} ")
         response = await make_get_request('film', params=test['parameter'])
         assert response.status == int(test['status']), test
-        # logger.info('pass response test'), test
         assert len(response.body) == int(test['lenght']), test
-        # logger.info('response lenght is ok')
-        # logger.debug(f'response body : {response.body}')
         if test['body'] != '':
             assert response.body == await read_json_data('film/' + test['body']), test
         logger.info(f"{test['name']} passed")
@@ -47,16 +27,12 @@ async def test_film(make_get_request, read_json_data):
 
 @pytest.mark.asyncio
 async def test_get_film_by_id(make_get_request, read_json_data):
-    # Имя файла надо убрать в конфиг
     testsconfig = await read_json_data('film/config_get_by_id.json')
     for test in testsconfig:
         logger.info(f"start test : {test['name']} ")
         response = await make_get_request('film/' + test['film_id'])
         assert response.status == int(test['status']), test
-        # logger.info('pass response test'), test
         assert len(response.body) == int(test['lenght']), test
-        # logger.info('response lenght is ok')
-        # logger.debug(f'response body : {response.body}')
         if test['body'] != '':
             assert response.body == await read_json_data('film/' + test['body']), test
         logger.info(f"{test['name']} passed")
@@ -64,16 +40,12 @@ async def test_get_film_by_id(make_get_request, read_json_data):
 
 @pytest.mark.asyncio
 async def test_film_search(make_get_request, read_json_data):
-    # Имя файла надо убрать в конфиг
     testsconfig = await read_json_data('film/config_search.json')
     for test in testsconfig:
         logger.info(f"start test : {test['name']} ")
         response = await make_get_request('film', method='/search', params=test['parameter'])
         assert response.status == int(test['status']), test
-        # logger.info('pass response test'), test
         assert len(response.body) == int(test['lenght']), test
-        # logger.info('response lenght is ok')
-        # logger.debug(f'response body : {response.body}')
         if test['body'] != '':
             assert response.body == await read_json_data('film/' + test['body']), test
         logger.info(f"{test['name']} passed")
