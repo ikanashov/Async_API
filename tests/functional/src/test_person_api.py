a6232326f0b8c27a0fbf6b10bd2647f7e4166303
Ivan Kanashov <ikanashov@gmail.com>
2021-04-25 00:10:36 +0500

    add tests for get film person by id

--- tests/functional/src/test_person_api.py
+++ tests/functional/src/test_person_api.py
@@ -40,3 +40,16 @@ async def test_film_search(make_get_request, read_json_data):
         if test['body'] != '':
             assert response.body == await read_json_data('person/' + test['body']), test
         logger.info(f"{test['name']} passed")
+
+
+@pytest.mark.asyncio
+async def test_person_film_by_id(make_get_request, read_json_data):
+    testsconfig = await read_json_data('person/config_get_person_film_by_id.json')
+    for test in testsconfig:
+        logger.info(f"start test : {test['name']} ")
+        response = await make_get_request('person/' + test['person_id'] + '/film')
+        assert response.status == int(test['status']), test
+        assert len(response.body) == int(test['lenght']), test
+        if test['body'] != '':
+            assert response.body == await read_json_data('person/' + test['body']), test
+        logger.info(f"{test['name']} passed")
