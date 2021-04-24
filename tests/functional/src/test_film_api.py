4e43d4a1d3a5683b7d35b55fcd978310d032adca
Ivan Kanashov <ikanashov@gmail.com>
2021-04-24 16:43:09 +0500

    add tests for get_all_film api

--- tests/functional/src/test_film_api.py
+++ tests/functional/src/test_film_api.py
@@ -8,34 +8,36 @@ from loguru import logger
 #   https://stackoverflow.com/questions/35206409/elasticsearch-2-1-result-window-is-too-large-index-max-result-window/35221900#35221900
 #   https://stackoverflow.com/questions/41677198/result-window-is-too-large-from-size-must-be-less-than-or-equal-to-10000-b
 #   curl -XPUT "http://localhost:9200/my_index/_settings" -d '{ "index" : { "max_result_window" : 500000 } }' -H "Content-Type: application/json"
-#   test filter[genre]
-#   test sort by imdb_rating
+#   + test filter[genre]
+#   + test sort by imdb_rating
 #   test page[size]
-#       size = default
+#      + size = default
 #       size = -1
 #       size = 0
 #       size = 1
-#       size = 50
+#       + size = 50
 #       size = 1000
 #   test page[number]
-#       number = default
-#       number = -1
+#       + number = default
+#       + number = -1
 #       number = 0
-#       number = 1
+#       + number = 1
 #       number = 1000
 # test search film
 # test get film by id
 
 @pytest.mark.asyncio
-async def test_film(make_get_request):
-    # Выполнение запроса
-    response = await make_get_request('film')
-
-    logger.debug('do response test')
-
-    # Проверка результата
-    assert response.status == 200
-    logger.info('pass response test')
-    assert len(response.body) == 50
-    logger.info('response lenght is ok')
-    #assert response.body == expected
+async def test_film(make_get_request, read_json_data):
+    # Имя файла надо убрать в конфиг
+    testsconfig = await read_json_data('film/config.json')
+    for test in testsconfig:
+        logger.info(f"start test : {test['name']} ")
+        response = await make_get_request('film', params = test['parameter'])
+        assert response.status == int(test['status']), test
+        #logger.info('pass response test'), test
+        assert len(response.body) == int(test['lenght']), test
+        #logger.info('response lenght is ok')
+        #logger.debug(f'response body : {response.body}')
+        if test['body'] != '':
+            assert response.body == await read_json_data('film/' + test['body']), test
+        logger.info(f"{test['name']} passed")
