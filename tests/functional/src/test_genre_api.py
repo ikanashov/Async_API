338f40e1b4d67dc7b490a885479e8d2013a35db2
Ivan Kanashov <ikanashov@gmail.com>
2021-04-25 12:42:59 +0500

    Add tests for get genre by id

--- tests/functional/src/test_genre_api.py
+++ tests/functional/src/test_genre_api.py
@@ -15,16 +15,15 @@ async def test_genre(make_get_request, read_json_data):
             assert response.body == await read_json_data('genre/' + test['body']), test
         logger.info(f"{test['name']} passed")
 
-"""
+
 @pytest.mark.asyncio
 async def test_get_person_by_id(make_get_request, read_json_data):
-    testsconfig = await read_json_data('person/config_get_by_id.json')
+    testsconfig = await read_json_data('genre/config_get_by_id.json')
     for test in testsconfig:
         logger.info(f"start test : {test['name']} ")
-        response = await make_get_request('person/' + test['person_id'])
+        response = await make_get_request('genre/' + test['person_id'])
         assert response.status == int(test['status']), test
         assert len(response.body) == int(test['lenght']), test
         if test['body'] != '':
-            assert response.body == await read_json_data('person/' + test['body']), test
+            assert response.body == await read_json_data('genre/' + test['body']), test
         logger.info(f"{test['name']} passed")
-"""
