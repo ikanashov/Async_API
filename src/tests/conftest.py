1a1df9a6b49f24868c9373857c1dfb94b6b95382
Ivan Kanashov <ikanashov@gmail.com>
2021-04-28 22:37:15 +0500

    add tests for elastic genres func

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,9 +4,7 @@ import os
 import sys
 from pathlib import Path
 
-import aiohttp
-
-from multidict import CIMultiDictProxy
+from loguru import logger
 
 import pytest
 
@@ -18,11 +16,18 @@ from elasticsearch import AsyncElasticsearch
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from core.config import config
 
+from services.film import FilmService
+
 from db import elastic
 from db import redis
 
 
 TEST_JSON_PATH = 'tests/data/'
+
+
+@pytest.fixture(scope='session')
+async def conf():
+    return config
 
 
 # https://github.com/pytest-dev/pytest-asyncio/issues/68
@@ -31,6 +36,7 @@ TEST_JSON_PATH = 'tests/data/'
 @pytest.fixture(scope='session')
 def event_loop():
     """Create an instance of the default event loop for each test case."""
+    logger.info('start event loop')
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
@@ -38,29 +44,41 @@ def event_loop():
 
 @pytest.fixture(scope='session')
 async def redis():
+    logger.info('start redis')
     redis.redis = await aioredis.create_redis_pool(
         (config.REDIS_HOST, config.REDIS_PORT),
         password=config.REDIS_PASSWORD,
         minsize=10, maxsize=20
     )
     yield redis.redis
+    logger.info('redis after yield')
     # await redis.redis.close()
 
 
 @pytest.fixture(scope='session')
 async def elastic():
+    logger.info('start elastic')
     elastic.es = AsyncElasticsearch(
         hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'],
         scheme=config.ELASTIC_SCHEME,
         http_auth=(config.ELASTIC_USER, config.ELASTIC_PASSWORD)
     )
-    yield elastic
-    # await elastic.es.close()
+    yield elastic.es
+    logger.info('elastic after yield')
+    await elastic.es.close()
+
+
+@pytest.fixture
+async def film_service(redis, elastic):
+    logger.info('start film_service')
+    film_service: FilmService = FilmService(redis, elastic)
+    return film_service
 
 
 # https://stackoverflow.com/questions/50329629/how-to-access-a-json-filetest-data-like-config-json-in-conftest-py
 @pytest.fixture
 async def read_json_data(request):
+    logger.info('start read json data')
     async def inner(datafilename: str) -> dict:
         jsonpath = Path(Path.cwd(), TEST_JSON_PATH, datafilename)
         with jsonpath.open() as fp:
--- src/tests/test_film.py
+++ src/tests/test_film.py
@@ -1,26 +1,59 @@
+import json
+
 from loguru import logger
 
 import pytest
 
 from services.film import FilmService
+from models.film import SFilmGenre
+
+@pytest.fixture(autouse=True)
+async def setup_genres(conf, elastic, read_json_data):
+    logger.info('setup genres')
+    datas = await read_json_data('es_genres_data.json')
+    body = ''
+    for data in datas:
+        index = {'index': {'_index': conf.ELASTIC_GENRE_INDEX, '_id': data['id']}}
+        body += json.dumps(index) + '\n' + json.dumps(data) + '\n'
+    results = await elastic.bulk(body)
+    logger.info(results)
+    yield datas
+    logger.info('genres after loop')
+    for data in datas:
+        await elastic.delete(index=conf.ELASTIC_GENRE_INDEX, id=data['id'])
+    logger.info('index cleared')
+
 
 
 @pytest.mark.asyncio
 async def test_service_is_up(redis, elastic):
     logger.info('start test services')
-    es = await elastic.es.cluster.health()
+    es = await elastic.cluster.health()
     assert es['status'] == 'green'
     assert await redis.ping() == b'PONG'
     logger.info('end test services')
 
 
 @pytest.mark.asyncio
-async def test_film_service(redis, elastic, read_json_data):
+async def test_film_service(film_service: FilmService, read_json_data):
     logger.info('test cache film service')
     data = await read_json_data('cache_test.json')
-    film_service: FilmService = FilmService(redis, elastic)
     assert film_service.cachekey(data['testkey']['input']) == data['testkey']['output']
     await film_service._put_data_to_cache(data['testputgetdata']['key'], data['testputgetdata']['text'])
     # get data return byte, maybe must return str
     assert await film_service._get_data_from_cache(data['testputgetdata']['key']) == bytes(data['testputgetdata']['text'], 'utf-8')
     logger.info('end test cache film service')
+
+
+@pytest.mark.asyncio
+async def test_genre_elastic(film_service: FilmService, read_json_data):
+    logger.info('test get data from genres index elastic')
+    datas = await read_json_data('es_genres_data.json')
+    for data in datas:
+        doc = await film_service._get_genre_from_elastic(data['id'])
+        logger.info(doc.dict())
+        logger.info(data)
+        assert doc.dict() == data
+        assert doc == SFilmGenre(**data)
+    assert None == await film_service._get_genre_from_elastic('not_found')
+    logger.info('end test genres index')
