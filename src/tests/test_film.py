e426f689d5483a19d815e4c7412f0bf6e78847ef
Ivan Kanashov <ikanashov@gmail.com>
2021-04-29 11:15:55 +0500

    tests for get genres in elastic

--- src/tests/test_film.py
+++ src/tests/test_film.py
@@ -7,6 +7,7 @@ import pytest
 from services.film import FilmService
 from models.film import SFilmGenre
 
+
 @pytest.fixture(autouse=True)
 async def setup_genres(conf, elastic, read_json_data):
     logger.info('setup genres')
@@ -47,7 +48,7 @@ async def test_film_service(film_service: FilmService, read_json_data):
 
 @pytest.mark.asyncio
 async def test_genre_elastic(film_service: FilmService, read_json_data):
-    logger.info('test get data from genres index elastic')
+    logger.info('test get single data from genres index elastic')
     datas = await read_json_data('es_genres_data.json')
     for data in datas:
         doc = await film_service._get_genre_from_elastic(data['id'])
@@ -56,4 +57,22 @@ async def test_genre_elastic(film_service: FilmService, read_json_data):
         assert doc.dict() == data
         assert doc == SFilmGenre(**data)
     assert None == await film_service._get_genre_from_elastic('not_found')
+    logger.info('end test genres index')
+
+
+@pytest.mark.asyncio
+async def test_genres_elastic(film_service: FilmService, read_json_data):
+    logger.info('test get many genres from elastic')
+    try:
+        await film_service._get_genres_from_elastic()
+    except Exception as err:
+        assert err.__class__ == TypeError, err
+
+    testsconfig = await read_json_data('config_es_genres.json')
+    for test in testsconfig:
+        datas = await film_service._get_genres_from_elastic(
+            page_size=test['page_size'], page_number=test['page_number'])
+        assert len(datas) == test['page_size'], datas
+        if test['body'] != '':
+            assert datas == await read_json_data(test['body']), test
     logger.info('end test genres index')
