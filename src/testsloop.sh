f27607ce863a170bcbb80487eaf3822ce1c1726b
Ivan Kanashov <ikanashov@gmail.com>
2021-05-03 12:35:28 +0500

    add all *.py to testsloop

--- src/testsloop.sh
+++ src/testsloop.sh
@@ -2,11 +2,12 @@
 
 echo "For run this loop needs entr util. apt install entr"
 
+source ./venv/bin/activate
+
 while true;
     do
-    ls db/*.py tests/*.py | entr -d sh -c \
+    ls models/*.py models/api/*.py db/*.py tests/*.py services/*.py api/v1/*.py core/*.py *.py  |\
+     entr -d sh -c \
     'clear && echo Changed && pytest -vv';
     # 'clear && echo Changed && pytest -s tests/test_film_es.py';
     done
-    
-    
