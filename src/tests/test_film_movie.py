1f0b6211ee6f2e56427170465c79f4168f51391e
Ivan Kanashov <ikanashov@gmail.com>
2021-05-07 23:53:49 +0500

    Movie test refactoring

--- src/tests/test_film_movie.py
+++ src/tests/test_film_movie.py
@@ -44,6 +44,7 @@ async def storage(elastic):
     yield storage
     logger.info('end setup storage')
 
+
 @pytest.fixture(scope='module')
 async def cache(redis):
     logger.info('setup cache')
@@ -53,6 +54,7 @@ async def cache(redis):
     yield cache
     logger.info('end setup cache')
 
+
 @pytest.fixture(scope='module')
 async def movie(conf, storage: es_db.ElasticStorage(), cache: Cache):
     logger.info('setup movie class')
@@ -61,6 +63,7 @@ async def movie(conf, storage: es_db.ElasticStorage(), cache: Cache):
     await movie.set_movie_index(conf.ELASTIC_INDEX)
     yield movie
     logger.info('end setup movie class')
+
 
 @pytest.mark.asyncio
 async def test_film_Movie_Class(conf, movie: mov_db.Movie, cache: Cache, redis, read_json_data):
@@ -72,17 +75,12 @@ async def test_film_Movie_Class(conf, movie: mov_db.Movie, cache: Cache, redis, 
         cache_doc = await cache.get_data(index=conf.ELASTIC_INDEX, id=data['id'])
         assert doc == SFilm(**data)
         assert doc == SFilm(**cache_doc)
-    assert await movie.get_film_by_id('not_found') is None
     logger.info('end test film movie class')
 
 
 @pytest.mark.asyncio
 async def test_films_search_Movie_Class(movie: mov_db.Movie, read_json_data):
     logger.info('test films search movie')
-    try:
-        await movie.search_film()
-    except Exception as err:
-        assert err.__class__ == TypeError, err
     testsconfig = await read_json_data('movie_search.json')
     for test in testsconfig:
         logger.info(f"start test : {test['name']} ")
@@ -97,10 +95,6 @@ async def test_films_search_Movie_Class(movie: mov_db.Movie, read_json_data):
 @pytest.mark.asyncio
 async def test_films_get_all_film_Movie_Class(movie: mov_db.Movie, read_json_data):
     logger.info('test films get_all movie')
-    try:
-        await movie.get_all_film()
-    except Exception as err:
-        assert err.__class__ == TypeError, err
     testsconfig = await read_json_data('movie_get_all_film.json')
     for test in testsconfig:
         logger.info(f"start test : {test['name']} ")
@@ -110,3 +104,22 @@ async def test_films_get_all_film_Movie_Class(movie: mov_db.Movie, read_json_dat
             assert docs == [SFilm(**doc) for doc in await read_json_data('movie/' + test['body'])]
         logger.info(f"{test['name']} passed")
     logger.info('end test films get_all movie')
+
+
+@pytest.mark.asyncio
+async def test_films_bad_parameter_Movie_class(movie: mov_db.Movie):
+    logger.info('start test bad parameter')
+    assert await movie.get_film_by_id('not_found') is None
+    try:
+        await movie.get_film_by_id()
+    except Exception as err:
+        assert err.__class__ == TypeError, err
+    try:
+        await movie.search_film()
+    except Exception as err:
+        assert err.__class__ == TypeError, err
+    try:
+        await movie.get_all_film()
+    except Exception as err:
+        assert err.__class__ == TypeError, err
+    logger.info('end test bad parameter')
