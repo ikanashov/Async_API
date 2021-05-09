bceb85d899191d25d8a3b615cd71d648d37de985
Ivan Kanashov <ikanashov@gmail.com>
2021-05-09 12:51:52 +0500

    fix Movie Class

--- src/services/movie.py
+++ src/services/movie.py
@@ -4,19 +4,16 @@ from models.elastic import ESFilterGenre, ESQuery
 from models.film import SFilm
 from models.interface import AbstractDataStore, AbstractMovie
 
-# Переделать
-datastore: AbstractDataStore = None
-
 
 class Movie(AbstractMovie):
-    def __init__(self) -> None:
+    def __init__(self, datastore: AbstractDataStore) -> None:
         self.datastore = datastore
 
     def set_movie_index(self, movieindex: str) -> None:
         self.movieindex = movieindex
 
     async def get_film_by_id(self, film_id: str) -> Optional[SFilm]:
-        movie = await datastore.get_by_id(self.movieindex, film_id)
+        movie = await self.datastore.get_by_id(self.movieindex, film_id)
         if movie:
             return SFilm(**movie)
 
@@ -29,7 +26,7 @@ class Movie(AbstractMovie):
         if genre_filter is not None:
             genre_filter = ESFilterGenre(query={'term': {'genre': {'value': genre_filter}}}).json()
 
-        movies = await datastore.search(
+        movies = await self.datastore.search(
             self.movieindex,
             page_size=page_size, page_number=page_number,
             sort=sort, body=genre_filter
@@ -42,7 +39,7 @@ class Movie(AbstractMovie):
         query: str, page_size: int, page_number: int
     ) -> Optional[List[SFilm]]:
         body = ESQuery(query={'multi_match': {'query': query}}).json(by_alias=True)
-        movies = await datastore.search(
+        movies = await self.datastore.search(
             self.movieindex,
             page_size=page_size, page_number=page_number,
             sort=None, body=body
