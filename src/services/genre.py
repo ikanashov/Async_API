40ddef0846cf8780d42963016711fbe1307b807c
Ivan Kanashov <ikanashov@gmail.com>
2021-05-09 12:55:40 +0500

    fix Genre class

--- src/services/genre.py
+++ src/services/genre.py
@@ -3,19 +3,16 @@ from typing import List, Optional
 from models.film import SFilmGenre
 from models.interface import AbstractDataStore, AbstractGenre
 
-# Переделать
-datastore: AbstractDataStore = None
-
 
 class Genre(AbstractGenre):
-    def __init__(self) -> None:
+    def __init__(self, datastore: AbstractDataStore) -> None:
         self.datastore = datastore
 
     def set_genre_index(self, genreindex: str) -> None:
         self.genreindex = genreindex
 
     async def get_genre_by_id(self, genre_id: str) -> Optional[SFilmGenre]:
-        genre = await datastore.get_by_id(self.genreindex, genre_id)
+        genre = await self.datastore.get_by_id(self.genreindex, genre_id)
         if genre:
             return SFilmGenre(**genre)
 
@@ -25,7 +22,7 @@ class Genre(AbstractGenre):
         page_size: int, page_number: int
     ) -> Optional[List[SFilmGenre]]:
 
-        genres = await datastore.search(
+        genres = await self.datastore.search(
             self.genreindex,
             page_size=page_size, page_number=page_number,
             sort=sort, body='{"query": {"match_all": {}}}'
